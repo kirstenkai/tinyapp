082826bb6648f2995901a23a27800d3b2fb78636
Kc Kai <kirstenckai@gmail.com>
2020-03-16 23:31:15 +0000

    Add a route handler for /urls

--- express_server.js
+++ express_server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 5000; // port 8080 wasn't working
 
+// set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// user res.render to load up an ejs view file
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
