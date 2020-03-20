19149b1b0d4fe809cb8d54629d28c7bb2b4aabde
Kc Kai <kirstenckai@gmail.com>
2020-03-20 21:59:07 +0000

    Create validation password function

--- helpers.js
+++ helpers.js
@@ -1,14 +1,35 @@
 
+// let users = {
+//   "userRandomID": {
+//     id: "userRandomID",
+//     email: "user@example.com",
+//     password: "purple-monkey-dinosaur",
+//   },
+//   "user2RandomID": {
+//     id: "user2RandomID",
+//     email: "user2@example.com",
+//     password: "dishwasher-funk"
+//   }
+// };
+
 const checkEmail = (users, email) => {
   for (let username in users) {
     if (users[username].email === email) {
       return true;
-    } 
+    }
     return false;
   }
 };
 
-module.exports = { checkEmail }
 
+const validatePassword = (users, password) => {
+  for (let user in users) {
+    if (users[user].password === password) {
+      return true;
+    }
+    return false;
+  }
+};
 
+module.exports = { checkEmail, validatePassword };
 
