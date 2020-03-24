55ce9642f0e4fbfe04ea8e1ccad3e369dc53bf15
Kc Kai <kirstenckai@gmail.com>
2020-03-24 02:24:22 +0000

    Delete test database

--- helpers.js
+++ helpers.js
@@ -1,17 +1,4 @@
 
-// let users = {
-//   "userRandomID": {
-//     id: "userRandomID",
-//     email: "user@example.com",
-//     password: "purple-monkey-dinosaur",
-//   },
-//   "user2RandomID": {
-//     id: "user2RandomID",
-//     email: "user2@example.com",
-//     password: "dishwasher-funk"
-//   }
-// };
-
 const checkEmail = (users, email) => {
   for (let username in users) {
     if (users[username].email === email) {
@@ -25,30 +12,46 @@ const findUser = (users, id) => {
   return id ? users[id] : undefined
 }
 
-const findUserByEmail = (users, email) => { 
-    for (let id in users) {
-      const user = users[id];
-      if (user.email === email) {
-        return user;
-      }
+const findUserByEmail = (users, email) => {
+  for (let id in users) {
+    const user = users[id];
+    if (user.email === email) {
+      return user;
     }
   }
-  
+}
 
+const urlsForUser = (id) => {
+  // returns the URLs where the userID is equal to the id of the currently logged-in user.
+  let usersDatabase = {};
+  for (url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      usersDatabase[url] = urlDatabase[url]
+    }
+  }
+  return usersDatabase;
 
+};
 
 const validatePassword = (users, password) => {
   for (let user in users) {
-    if (users[user].password === password) {
+    if ((users[user].password) === password) {
       return true;
     }
     return false;
   }
 };
 
+
 const verifyLogin = (users, email, password) => {
   return checkEmail(users, email) && validatePassword(users, password)
 }
 
-module.exports = { checkEmail, validatePassword, findUser, verifyLogin, findUserByEmail };
-
+module.exports = {
+  checkEmail,
+  validatePassword,
+  findUser,
+  verifyLogin,
+  findUserByEmail,
+  urlsForUser,
+};
